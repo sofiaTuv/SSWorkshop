c98607d5b7e191b1631dcdcefcbc6aaa18a1dacf
Sofia Tuvykina <hdfaa.56@gmail.com>
2024-03-21 16:52:44 +0300

    добавила pageObject

--- test/testAddCustomer.py
+++ test/testAddCustomer.py
@@ -1,28 +1,24 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
+from helper import Helper
+from page.methods_page import MethodsPage
 
-from helper import generate_code, generate_name, generate_last_name
 
 URL = 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager'
 
-browser = webdriver.Chrome()
-browser.get(URL)
-button = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable((By.XPATH, '//button[@ng-class="btnClass1"]')))
-button.click()
 
+class TestAddCustomer:
+    def test_add_customer(self, browser):
+        page = MethodsPage(browser)
+        browser.get(URL)
+        page.click_button()
 
-postcode = generate_code()
-postCode = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable((By.XPATH,'//input[@ng-model="postCd"]'))).send_keys(postcode)
+        postcode = Helper.generate_code()
 
-fname = generate_name(postcode)
-firstName = browser.find_element(By.XPATH,'//input[@ng-model="fName"]').send_keys(fname)
+        page.enter_postcode(postcode)
 
-lname = generate_last_name()
-lastName = browser.find_element(By.XPATH, '//input[@ng-model="lName"]').send_keys(lname)
+        fname = Helper.generate_name(postcode)
+        page.enter_first_name(fname)
 
+        lname = Helper.generate_last_name()
+        page.enter_last_name(lname)
 
-
+        page.click_add_button()
