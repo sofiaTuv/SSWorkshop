9b4c0bbdbe85e8141f3f6e002c12aaffc5b14ce0
Sofia Tuvykina <hdfaa.56@gmail.com>
2024-03-21 16:52:03 +0300

    доработала generate_name

--- helper.py
+++ helper.py
@@ -4,23 +4,24 @@ from faker import Faker
 faker = Faker()
 
 
-def generate_code():
-    return faker.random_number(digits=10)
+class Helper:
 
+    @staticmethod
+    def generate_code():
+        return faker.random_number(digits=10)
 
-def generate_last_name():
-    return faker.last_name()
+    @staticmethod
+    def generate_last_name():
+        return faker.last_name()
 
-
-def generate_name(postcode):
-    alphabet = string.ascii_lowercase
-    result = ''
-    postcode_str = str(postcode)
-    # разбиваем на двузначные цифры
-    for i in range(0, len(str(postcode_str)), 2):
-        code = postcode_str[i:i + 2]
-        # проверяем, является ли символ числом
-        if code.isdigit():
-            index = int(i) % 26
-            result += alphabet[index]
-    return result
+    @staticmethod
+    def generate_name(postcode):
+        alphabet = string.ascii_lowercase
+        result = ''
+        postcode_str = str(postcode)
+        # разбиваем на двузначные цифры
+        for i in range(0, len(postcode_str), 2):
+            digit = int(postcode_str[i:i + 2])
+            result += alphabet[digit % 26]
+        first_name = result.capitalize()
+        return first_name
