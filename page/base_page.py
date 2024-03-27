957f6cf1527e1fe08bd9e5497261be2842d5719b
Sofia Tuvykina <hdfaa.56@gmail.com>
2024-03-27 12:49:41 +0300

    изменила url

--- page/base_page.py
+++ page/base_page.py
@@ -3,10 +3,17 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
+    PAGE_URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager"
 
     def __init__(self, browser):
         self.browser = browser
 
+    def open(self):
+        self.browser.get(self.PAGE_URL)
+
     def wait_until(self, path):
         WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(path))
         return self.browser.find_element(*path)
+
+    def alert_is_present(self):
+        return WebDriverWait(self.browser, 5).until(EC.alert_is_present())
