30ca329ca523ed8497472088a671cc94b93e7bd2
Sofia Tuvykina <hdfaa.56@gmail.com>
2024-03-27 12:52:14 +0300

    добавила xpath

--- page/paths.py
+++ page/paths.py
@@ -2,8 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class Paths:
-    BUTTON = (By.XPATH, '//button[@ng-class="btnClass1"]')
+    BUTTON_ADD_CUST = (By.XPATH, '//button[@ng-class="btnClass1"]')
     POSTCODE = (By.XPATH, '//input[@ng-model="postCd"]')
     FIRST_NAME = (By.XPATH, '//input[@ng-model="fName"]')
     LAST_NAME = (By.XPATH, '//input[@ng-model="lName"]')
     ADD_BUTTON = (By.XPATH, '//button[@type="submit"]')
+    CUSTOMERS = (By.XPATH, '//button[@ng-class="btnClass3"]')
